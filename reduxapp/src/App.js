e916e4052eed0ce34aa530a6cd104fadcb2cda2c
zubairkhan1234 <zubairsaleem1210@gmail.com>
2021-08-23 23:35:09 +0500

    just create react App for learning react

---
+++ reduxapp/src/App.js
@@ -0,0 +1,18 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <div>
+        <h1>
+          add yout text
+        </h1>
+      </div>
+      <div>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
